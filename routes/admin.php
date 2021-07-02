d1ae5b411555d6d00184f03aa8f83c116023e676
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-07-02 11:11:42 +0700

    First commit

---
+++ routes/admin.php
@@ -0,0 +1,6 @@
+<?php
+//routes/admin.php
+
+$f3->route('GET /admin', function(){
+    echo 'Hello from admin page!';
+});
---
+++ routes/index.php
@@ -0,0 +1,6 @@
+<?php
+//routes/index.php
+
+$f3->route('GET /', function(){
+    echo 'Hello, world!';
+});
