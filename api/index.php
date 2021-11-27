107c27b64d2a6e2ed887406eabdf64fa7600830c
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-11-27 15:51:21 +0700

    Make it better

---
+++ api/index.php
@@ -0,0 +1,27 @@
+<?php
+// Kickstart the framework
+require('vendor/autoload.php');
+$f3 = \Base::instance();
+
+session_start();
+
+require('tool.php');
+$tool = new Tool();
+$localhost = $tool->is_localhost();
+
+if($localhost){
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
+
+$db_url = $_ENV["DATABASE_URI"];
+
+$client = new MongoDB\Client($db_url);
+$db = $client->multimedia;
+
+$f3->set('DB', $db);
+
+require_once('routes/index.php');
+require_once('routes/admin.php');
+
+$f3->run();
